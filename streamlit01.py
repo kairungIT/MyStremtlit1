54a28a0decbbcb3761f8ff271a3feb5b4b00c8cd
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-17 10:34:41 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.title('การทดสอบการเขียนเว็บด้วย Streamlit')
+col1, col2 = st.columns(2)
+col1.write("This is column 1")
+col2.write("This is column 2")
+
 st.header('ไก้รุ่ง เฮงพระพรหม')
 st.subheader('My sub')
