deb591c1b396ce25aaef27fbecd342f29fdc1665
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-17 10:42:35 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -5,10 +5,13 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-   st.header("ไก้รุ่ง เฮงพระพรหม")
-with col1:
-   st.header("Versicolor")
    st.image("./pic/kairung01.jpg")
+with col1:
+   #st.header("Versicolor")
+   st.header("ไก้รุ่ง เฮงพระพรหม")
+   st.subheader('สาขาวิชาวิทยาการข้อมูล')
+   st.subheader('คณะวิทยาศาสตร์และเทคโนโลยี')
+   st.subheader('มหาวิทยาลัยราชภัฏนครปฐม')
 
 st.header('ไก้รุ่ง เฮงพระพรหม')
 st.subheader('My sub')
