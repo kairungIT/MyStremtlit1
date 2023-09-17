0e0a86c27793e8a68f9cd9063fa553c7e07db282
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-17 10:45:16 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -5,7 +5,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-   st.image("./pic/kairung01.jpg")
+   st.image("./pic/kairung02.jpg")
 with col1:
    #st.header("Versicolor")
    st.header("ไก้รุ่ง เฮงพระพรหม")
