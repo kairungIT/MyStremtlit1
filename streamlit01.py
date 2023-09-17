13243d0c193232239427930e118a2f06ee5c3c8a
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-17 10:48:12 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -6,7 +6,7 @@ col1, col2 = st.columns(2)
 #col2.write("This is column 2")
 with col1:
    st.image("./pic/kairung02.jpg")
-with col1:
+with col2:
    #st.header("Versicolor")
    st.header("ไก้รุ่ง เฮงพระพรหม")
    st.text('สาขาวิชาวิทยาการข้อมูล')
