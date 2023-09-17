9b9698c68bbd8be27dc8c3e1214b5a562cf1ae4d
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-09-17 10:38:21 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -2,8 +2,13 @@ import streamlit as st
 
 st.title('การทดสอบการเขียนเว็บด้วย Streamlit')
 col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+   st.header("ไก้รุ่ง เฮงพระพรหม")
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/kairung01.jpg")
 
 st.header('ไก้รุ่ง เฮงพระพรหม')
 st.subheader('My sub')
